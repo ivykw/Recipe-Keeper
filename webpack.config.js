f9607fa49f52d6b5e9a7ecf6706ac044e0d607c4
ivykw <ivyw219@gmail.com>
2022-05-05 13:08:20 -0500

    fixed a file path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: '.client/src/index.jsx',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
